40062f6c476a1b45af4a29b4cbddaf40f480ec17
Danilo Batson <>
2022-01-27 04:29:48 -0800

    feat: Adds styles to Banner component

--- components/banner/banner.js
+++ components/banner/banner.js
@@ -6,11 +6,33 @@ export const Banner = ({ imgUrl, title, subtitle }) => {
     alert('Handle On Play ')
   )
   return (
-    <>
-    <h3>{title}</h3>
-    <h5>{subtitle}</h5>
-      <button onClick={handleOnPlay}>Play Button</button>
-    <div style={{ backgroundImage: `url(${imgUrl})`, width: '100%', height: '100%', position: 'absolute', backgroundSize: 'cover', backgroundPosition: '50% 50%' }}></div>
-  </>
-    )
+    <div className={styles.container}>
+
+      <div className={styles.leftWrapper}>
+        <div className={styles.left}>
+
+          <div className={styles.nseriesWrapper}>
+            <p className={styles.firstLetter}>N</p>
+            <p className={styles.series}>S E R I E S</p>
+          </div>
+
+          <h3 className={styles.title}>{title}</h3>
+          <h5 className={styles.subtitle}>{subtitle}</h5>
+
+          <div className={styles.playBtnWrapper}>
+            <button onClick={handleOnPlay} className={styles.btnWithIcon}>
+              <span className={styles.playText}>Play Button</span></button>
+          </div>
+
+        </div>
+      </div>
+
+      <div
+        className={styles.bannerImg}
+        style={{
+          backgroundImage: `url(${imgUrl}`,
+        }}
+      ></div>
+    </div>
+  )
 }
