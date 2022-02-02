a0a79895d405e75969b539cbeb4837b45bb5befb
Danilo Batson <djbatson19@gmail.com>
2022-02-02 00:33:45 -0800

    feat: set image error handler on Card component

--- components/card/card.js
+++ components/card/card.js
@@ -19,7 +19,9 @@ export const Card = ({ imgUrl, size = 'medium'}) => {
   const handleOnError = () => {
     setImgSrc(
       "https://images.unsplash.com/photo-1485846234645-a62644f84728?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1340&q=80"
-    );  }
+    );
+  }
+  
   return (
     <div className={styles.container}>
       <div className={classMap[size]}>
