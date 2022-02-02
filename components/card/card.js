df2579c21fc01a2d678c83bb3d6e3fc7ac60eea4
Danilo Batson <djbatson19@gmail.com>
2022-02-02 00:41:46 -0800

    feat: adds hover to card component

--- components/card/card.js
+++ components/card/card.js
@@ -21,12 +21,18 @@ export const Card = ({ imgUrl, size = 'medium'}) => {
       "https://images.unsplash.com/photo-1485846234645-a62644f84728?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1340&q=80"
     );
   }
-  
+
+  const scale = id === 0 ? {scaleY: 1.1} : {scale: 1.1}
+
+  const shouldHover = shouldScale && {
+    whileHover: {...scale}
+  }
+
   return (
     <div className={styles.container}>
-      <div className={classMap[size]}>
+      <motion.div className={cls(styles.imgMotionWrapper, classMap[size])} {...shouldHover} >
         <Image src={imgSrc} onError={handleOnError} layout='fill' alt="img" className={styles.cardImg}/>
-      </div>
+      </motion.div >
     </div>
   )
 }
